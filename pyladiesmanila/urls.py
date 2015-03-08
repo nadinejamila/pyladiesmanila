7274ba1d639d900afc17017700a081aeeb711aa5
Nadine Jamila <nadinejamila@gmail.com>
2015-03-09 01:49:43 +0800

    Redirect home url to event page

--- pyladiesmanila/urls.py
+++ pyladiesmanila/urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('',
     # Examples:
     #url(r'^$', 'pyladiesmanila.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+    url(r'^$', 'events.views.home', name='home'),
     url(r'^events/', include('events.urls')),
     #url(r'^admin/', include(admin.site.urls)),
 )
